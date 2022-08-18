31cd5220d1de13c81f9c52b103bce0f44f77ff50
HugoLorent <77060035+HugoLorent@users.noreply.github.com>
2022-08-18 15:19:26 +0200

    finished roman-numeral-converter.js
    Signed-off-by: HugoLorent <77060035+HugoLorent@users.noreply.github.com>

--- roman-numeral-converter.js
+++ roman-numeral-converter.js
@@ -1,17 +1,122 @@
-/**
- * @param {number} num
- */
 function convertToRoman(num) {
-  const numStr = num.toString();
+  const finalArray = [];
+  const numStrArray = num.toString().split('');
+  let isNumberFinished = false;
+
+  while (isNumberFinished === false) {
+    switch (numStrArray.length) {
+      case 1:
+        finalArray.push(unitsTransform(numStrArray[numStrArray.length - 1]));
+        isNumberFinished = true;
+        break;
+      case 2:
+        finalArray.push(tensTransform(numStrArray[numStrArray.length - 2]));
+        numStrArray.splice(numStrArray.length - 2, 1);
+        break;
+      case 3:
+        finalArray.push(hundredsTransform(numStrArray[numStrArray.length - 3]));
+        numStrArray.splice(numStrArray.length - 3, 1);
+        break;
+      case 4:
+        finalArray.push(
+          thousandsTransform(numStrArray[numStrArray.length - 4])
+        );
+        numStrArray.splice(numStrArray.length - 4, 1);
+        break;
+      default:
+        return 'num must be between 1 and 3999';
+    }
+  }
+  return finalArray.join('');
 }
 
-function unitsTransform(num) {}
+function unitsTransform(numStr) {
+  switch (numStr) {
+    case '1':
+      return 'I';
+    case '2':
+      return 'II';
+    case '3':
+      return 'III';
+    case '4':
+      return 'IV';
+    case '5':
+      return 'V';
+    case '6':
+      return 'VI';
+    case '7':
+      return 'VII';
+    case '8':
+      return 'VIII';
+    case '9':
+      return 'IX';
+    default:
+      break;
+  }
+}
 
-function tensTransform(num) {}
+function tensTransform(numStr) {
+  switch (numStr) {
+    case '1':
+      return 'X';
+    case '2':
+      return 'XX';
+    case '3':
+      return 'XXX';
+    case '4':
+      return 'XL';
+    case '5':
+      return 'L';
+    case '6':
+      return 'LX';
+    case '7':
+      return 'LXX';
+    case '8':
+      return 'LXXX';
+    case '9':
+      return 'XC';
+    default:
+      break;
+  }
+}
 
-function hundredsTransform(num) {}
+function hundredsTransform(numStr) {
+  switch (numStr) {
+    case '1':
+      return 'C';
+    case '2':
+      return 'CC';
+    case '3':
+      return 'CCC';
+    case '4':
+      return 'CD';
+    case '5':
+      return 'D';
+    case '6':
+      return 'DC';
+    case '7':
+      return 'DCC';
+    case '8':
+      return 'DCCC';
+    case '9':
+      return 'CM';
+    default:
+      break;
+  }
+}
 
-function thousandsTransform(num) {}
+function thousandsTransform(numStr) {
+  switch (numStr) {
+    case '1':
+      return 'M';
+    case '2':
+      return 'MM';
+    case '3':
+      return 'MMM';
+    default:
+      break;
+  }
+}
 
 console.log(convertToRoman(2));
 console.log(convertToRoman(3));
