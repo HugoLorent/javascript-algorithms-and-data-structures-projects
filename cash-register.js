104f2471bf9d51235285006b269118dadd84f55c
HugoLorent <77060035+HugoLorent@users.noreply.github.com>
2022-09-06 20:11:03 +0200

    wip
    Signed-off-by: HugoLorent <77060035+HugoLorent@users.noreply.github.com>

--- cash-register.js
+++ cash-register.js
@@ -16,25 +16,64 @@ function checkCashRegister(price, cash, cid) {
     change: [],
   };
 
-  const priceDifference = cash - price;
+  let priceAndPaymentDifference = cash - price;
   const cidSum = cid.reduce((acc, val) => {
     if (!isNaN(acc + val[1])) {
       return acc + val[1];
     }
   }, 0);
 
-  if (cidSum - priceDifference < 0) {
+  if (cidSum - priceAndPaymentDifference < 0) {
     return (change = {
       status: 'INSUFFICIENT_FUNDS',
       change: [],
     });
   }
-  if (cidSum - priceDifference === 0) {
+
+  if (cidSum - priceAndPaymentDifference === 0) {
     return (change = {
       status: 'CLOSED',
       change: cid,
     });
   }
+
+  const onlyAvailablesEquivalencies = findOnlyAvailablesEquivalencies(
+    equivalencies,
+    priceAndPaymentDifference
+  );
+
+  while (priceAndPaymentDifference !== 0) {
+    const moneyToSubstract =
+      onlyAvailablesEquivalencies[onlyAvailablesEquivalencies.length - 1][1];
+
+    if (cid[onlyAvailablesEquivalencies.length - 1][1] - moneyToSubstract < 0) {
+      onlyAvailablesEquivalencies.splice(
+        onlyAvailablesEquivalencies.length - 1,
+        1
+      );
+    }
+    cid[onlyAvailablesEquivalencies.length - 1][1] -= moneyToSubstract;
+    priceAndPaymentDifference = priceAndPaymentDifference - moneyToSubstract;
+  }
+
+  return (change = {
+    status: 'OPEN',
+    change: cid,
+  });
+}
+
+function findOnlyAvailablesEquivalencies(
+  equivalencies,
+  priceAndPaymentDifference
+) {
+  const availablesEquivalencies = [];
+
+  for (let i = 0; i < equivalencies.length; i++) {
+    if (equivalencies[i][1] <= priceAndPaymentDifference) {
+      availablesEquivalencies.push(equivalencies[i]);
+    }
+  }
+  return availablesEquivalencies;
 }
 
 console.log(
@@ -50,6 +89,19 @@ console.log(
     ['ONE HUNDRED', 100],
   ])
 );
+// console.log(
+//   checkCashRegister(3.26, 100, [
+//     ['PENNY', 1.01],
+//     ['NICKEL', 2.05],
+//     ['DIME', 3.1],
+//     ['QUARTER', 4.25],
+//     ['ONE', 90],
+//     ['FIVE', 55],
+//     ['TEN', 20],
+//     ['TWENTY', 60],
+//     ['ONE HUNDRED', 100],
+//   ])
+// );
 // console.log(
 //   checkCashRegister(19.5, 20, [
 //     ['PENNY', 0.5],
